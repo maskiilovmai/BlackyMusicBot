14866087d1f969d91d4bde3a69e6926c87daafd2
maskiilovmai <maikimlong2405@gmail.com>
2021-11-26 18:51:28 +0700

    lavalink

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ module.exports = {
 
   nodes: {
      
-      host: "lavlink.maskiilovmai.repl.co",
+      host: "lavalink.maskiilovmai.repl.co",
       port: 443,
       password: "maskiilovmai",
       id: "Sakura",
