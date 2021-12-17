cc34c67128ae054efdcce31335a40a4aea7a53f0
maskiilovmai <maikimlong2405@gmail.com>
2021-12-17 21:51:11 +0700

    add server.js for hosting on Repl

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const MusicBot = require("./src/structures/MusicClient");
 const client = new MusicBot();
 
+const keepAlive = require("./server")
+
 client.connect()
 
 module.exports = client; 
+
+keepAlive()
