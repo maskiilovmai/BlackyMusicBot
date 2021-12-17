ff36209089e7accf4c859db6d80a2a91b02a0df5
maskiilovmai <maikimlong2405@gmail.com>
2021-12-17 22:09:12 +0700

    remove something

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const MusicBot = require("./src/structures/MusicClient");
 const client = new MusicBot();
 
-const keepAlive = require("./server")
 
 client.connect()
 
 module.exports = client; 
-
-keepAlive()
